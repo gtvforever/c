fd6e23dccf9aac8dfc5594a420a5043540910a8a
guoming <gtvforever@gmail.com>
2013-07-09 22:23:39 +0800

    optimize the string reverse algorithm

--- string/reverse_string.c
+++ string/reverse_string.c
@@ -1,70 +1,6 @@
 #include <string.h>
-
+#include <stdio.h>
 static char name[] = "cisco is an great company!";
-
-
-void reverse_stringerse_string(char* p)
-{
-	int len;
-	int i;
-	char* blank_pos;
-	char* rev_str_1 = NULL;
-	char* rev_str_2 = NULL;
-
-	char* init_pos_1 = NULL;
-	char* init_pos_2 = NULL;
-	int offset = 0;
-
-	if(NULL == p)
-		return NULL;
-
-	len = strlen(p);
-	rev_str_1 = (char*)malloc(len*sizeof(char) + 1);
-	rev_str_2 = (char*)malloc(len*sizeof(char) + 1);
-	init_pos_1 = rev_str_1;
-	init_pos_2 = rev_str_2;
-
-	if(rev_str_1 == NULL || rev_str_2 == NULL)
-		return NULL;
-
-	for(i = 0 ; i < len; i++)
-	{
-		rev_str_1[i] = name[len - i - 1];
-	}
-	rev_str_1[len] = '\0';
-
-	printf("rev_str_1 is>>> %s\n", rev_str_1);
-
-	while(1)
-	{
-		blank_pos = strchr(init_pos_1 , ' ');
-
-		if(blank_pos == NULL)
-		{
-			break;
-		}
-		offset = blank_pos - init_pos_1;
-
-		for (i = 0; i < offset; i++)
-		{
-			init_pos_2[i] = init_pos_1[offset - i - 1];
-		}
-		init_pos_2[offset] = ' ';
-		init_pos_1 += offset + 1;
-		init_pos_2 += offset + 1;
-	}
-
-	offset = strlen(init_pos_1);
-	for (i = 0 ; i < offset; i++)
-	{
-		init_pos_2[i] = init_pos_1[offset - i - 1];
-	}
-
-	printf("rev_str_2 is>>> %s\r\n", rev_str_2 );
-
-	free(rev_str_1);
-	free(rev_str_2);
-}
 
 
 
@@ -91,6 +27,28 @@ void myrevert(char* src)
 
 
 }
+
+
+void myrevert2(char* src)
+{
+	int end = strlen(src);
+
+	int begin = 0;
+	char temp;
+	
+	if(end == 1)
+		return;
+
+	while(begin < end)
+	{
+		temp = src[end];
+		src[end] = src[begin];
+		src[begin] = temp;
+		end--;
+		begin++;
+	}
+
+}
 int main()
 {
 	char str[10] = "abcdefgh";
@@ -99,7 +57,7 @@ int main()
 	/* { */
 		/* printf("%x\n", str[i]); */
 	/* } */
-	myrevert(str);
+	myrevert2(str);
 	/* for(i = 0; i < 10; i++) */
 	/* { */
 		/* printf("%x\n", str[i]); */
